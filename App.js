42043e69269b973082bc4261ea58df3e627f4ace
Bayu Oktari <bayu.oktari@tanihub.com>
2022-07-16 21:48:04 +0700

    [ADD] initiate react native app

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, ScrollView} from 'react-native';
+
+export default function App() {
+  return (
+    <ScrollView>
+      <Text>Hello World</Text>
+      <Text>Apa Kabar</Text>
+    </ScrollView>
+  );
+}
